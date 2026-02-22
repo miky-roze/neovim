96e1d889e01965d56a3a6d532b4b3eec0fe73016
miky-roze <mike@macbook.local>
2026-02-22 20:39:19 +0100

    fix tree-sitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,35 @@
 return {
 	{
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = { "OXY2DEV/markview.nvim" },
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.config")
 
-      configs.setup({
-          ensure_installed = { 
-						"lua",
-						"bash",
-						"dockerfile",
-						"go",
-						"gomod",
-						"gosum",
-						"gotmpl",
-						"gowork",
-						"json",
-						"yaml",
-						"make",
-						"sql",
-						"terraform"
-					},
-					auto_install = false,
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-    end
+			configs.setup({
+				ensure_installed = {
+					"lua",
+					"bash",
+					"dockerfile",
+					"go",
+					"gomod",
+					"gosum",
+					"gotmpl",
+					"gowork",
+					"json",
+					"yaml",
+					"make",
+					"sql",
+					"hcl",
+					"markdown",
+					"markdown_inline"
+				},
+				auto_install = false,
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end
 	}
 }
