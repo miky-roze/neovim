d6462701609f69738673979d1e926c22e411376d
miky-roze <mike@macbook.local>
2026-05-09 19:52:27 +0200

    add new opts and keymaps

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,11 +12,12 @@ vim.api.nvim_create_autocmd(
       local opts = { buffer = event.buf }
       vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
       vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-      vim.keymap.set("n", "gi", function() vim.lsp.buf.implementations() end, opts)
+      vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
       vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
       vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
       vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
       vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set("n", "<leader>cf", function() vim.lsp.buf.format() end, opts)
       vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
       vim.diagnostic.config({
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,8 @@
--- <Tab> will use shiftwidth setting
 vim.opt.smarttab = true
+vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
 
 -- display line number
 vim.opt.number = true
